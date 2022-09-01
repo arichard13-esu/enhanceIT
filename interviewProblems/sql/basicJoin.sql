b38890d6817bacab4f2909942d74e34537392278
Aaron <Aaron>
2022-09-01 18:00:02 -0400

    solution

--- interviewProblems/sql/basicJoin.sql
+++ interviewProblems/sql/basicJoin.sql
@@ -1,1 +1,12 @@
+SELECT
+    distinct p.name
+,   c.name
+FROM professor AS p
+inner join schedule as s
+on p.id = s.professor_id
+INNER JOIN course AS c
+ON s.course_id = c.id
 
+order by
+    p.name
+,   c.name
